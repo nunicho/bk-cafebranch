ef50e00fd863cc09dda680629ae47b25e88c412d
Mauricio-Alonso <conta.alonso@gmail.com>
2022-10-20 17:57:29 -0300

    Agregamos los middlewares necesarios

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import morgan from "morgan"
+import cors from 'cors'
+import path from 'path'
 
 // crear una instancia express (express tiene que ser con minúscula)
 
@@ -9,7 +12,25 @@ app.set('port', process.env.PORT ||  4000)
 
 //usar el puerto
 app.listen(app.get('port'),()=>{
-    console.log('Estoy en el puerto'+ app.get('port'));
+    console.log('Estoy en el puerto '+ app.get('port'));
 })
 
-console.log('Hola mundo')
+// middlewares: funciones que se ejecutan antes de las rutas
+// da información extra en la terminal 
+app.use(morgan('dev'));
+//permitir peticiones remotas
+app.use(cors());
+//middlewares para interpretar los objetos JSON 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+//cargar un archivo estático 
+app.use(express.static(path.join(__dirname, '../public')))
+console.log(path.join(__dirname, '../public'))
+
+//rutas: nombre de dominio + ---- 
+
+// http://localhost:4000/
+app.get('/productos', (req, res)=>{
+    res.send('Esto es una prueba de la petición GET')
+
+})
