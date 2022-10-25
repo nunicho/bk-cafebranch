8acdaa42287674080f783175eafb1292047a63b6
Mauricio-Alonso <conta.alonso@gmail.com>
2022-10-25 18:30:28 -0300

    Agregamos la logica para crear un producto y obtener 1 arreglo de todos los productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,12 +1,44 @@
-export const listarProductos = (req, res) =>{
-        res.send('aquí tengo que retornar un arreglo de productos')
+import Producto from '../models/producto'
+
+
+
+
+export const listarProductos = async (req, res) =>{
+try{
+     
+const listaProductos = await Producto.find();
+        // buscar todos los productos en la Base de Datos
+res.status(200).json(listaProductos)
+.send('Esto es una prueba de la petición POST')
+  } catch (error){
+    console.log(error);
+    res.status(404).json({
+    mensaje: ('Error al intentar agregar un producto')
+})
+}
+
+
         }
 
 
-export const crearProducto = (req, res)=>{
-// extraer del body los datos
+export const crearProducto = async(req, res)=>{
+
+try{
+        // extraer del body los datos
 console.log(req.body)
-// agregar la validación correspondiente
-// guardar los productos en la Base de Datos
-         res.send('Esto es una prueba de la petición POST')   
+        // agregar la validación correspondiente
+const productoNuevo = new Producto(req.body);
+        // guardar los productos en la Base de Datos
+productoNuevo.save()
+        // responder al usuario que todo está Bien.    
+res.status(201).json({
+    mensaje: 'El producto fue correctamente creado'
+})
+.send('Esto es una prueba de la petición POST')
+  } catch (error){
+    console.log(error);
+    res.status(400).json({
+    mensaje: ('Error al intentar agregar un producto')
+})
+}
 }
