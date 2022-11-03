0cf0618abcc0a77a837a96b4b7fed44e9c1667b2
Mauricio-Alonso <conta.alonso@gmail.com>
2022-11-03 17:04:27 -0300

    avanzando

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 //localhost: 127.0.0.1 
 
-const url = 'mongodb://localhost:27017/cafe-branch';
+const url = 'mongodb+srv://nunicho:qweasdzxc@cluster0.b8sgvyv.mongodb.net/cafe-branch';
 
 mongoose.connect(url);
 
